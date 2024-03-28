4746e71d7a938e119b22323e08b63ac81b6920fb
ifshad <shoronshark@gmail.com>
2024-03-28 17:33:10 +0600

    footer added

--- Components/Layout/Footer.tsx
+++ Components/Layout/Footer.tsx
@@ -1,9 +1,137 @@
+"use client"
 import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { FaDiscord, FaFacebook, FaLinkedinIn, FaTwitter } from 'react-icons/fa6'
+import Link from 'next/link'
+import { BiLogoFacebookSquare, BiLogoLinkedinSquare } from 'react-icons/bi'
 
 export default function Footer() {
+  const [loading, setLoading] = useState(false)
+  const [axiosError, setAxiosError] = useState<any>()
+  const [posted, setPosted] = useState(false)
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    const form = new FormData(e.currentTarget)
+    const name = form.get('name')
+    const email = form.get('email')
+    const message = form.get('message')
+
+    const messageData = {
+      Name: name,
+      Email: email,
+      Message: message,
+    }
+    setLoading(true)
+
+    axios
+      .post('https://api.sheetapi.rest/api/v1/sheet/qzdUq3c8fMqcse8FmhNRL', messageData)
+      .then(() => { })
+      .catch((error) => setAxiosError(error))
+      .finally(() => {
+        setLoading(false)
+        setPosted(true)
+      })
+    setTimeout(() => {
+      setPosted(false)
+    }, 3000)
+    e.target.reset()
+  }
   return (
-    <div className='container'>
-      Footer
+    <div className='container border-t-2'>
+      <div className='grid grid-cols-8 p-10'>
+        {/* Description */}
+        <div className='col-span-5'>
+          <div className='w-3/5 mx-auto'>
+            <h2 className='mb-3 text-3xl font-semibold text-base-300'>Lets Connect</h2>
+            <p className='mb-6 text-base-100'>Please fill out the form on this section to contact with me or call between 9:00 A.M and 8.00 P.M ET, Monday through Friday.</p>
+            <ul className='flex gap-3 items-center'>
+              <li className='text-base-100 hover:text-navy'><Link href="https://www.facebook.com/ifshadhasan.sharan"><BiLogoFacebookSquare size={30} /></Link></li>
+              <li className='text-base-100 hover:text-navy'><Link href="https://www.linkedin.com/in/ifshad-hasan-sharan-50a6b1178/"><BiLogoLinkedinSquare size={30} /></Link></li>
+              <li className='text-base-100 hover:text-navy'><Link href="/"><FaDiscord size={25} /></Link></li>
+            </ul>
+          </div>
+        </div>
+        {/* Form */}
+        <div className='col-span-3'>
+          <div className=''>
+            <div className='text-2xl text-teal font-semibold mb-8'>
+              Lets message me!
+            </div>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-y-5'>
+              <div className='relative'>
+                <input
+                  type='text'
+                  name='name'
+                  id='name'
+                  className='p-3 bg-transparent border border-gray-200 rounded-[.5rem] outline-none peer w-full placeholder-transparent focus:border-teal'
+                  placeholder='name'
+                  required
+                />
+                <label
+                  htmlFor='name'
+                  className='absolute left-3 -top-3 cursor-text bg-sage px-1 text-base lg:peer-focus:text-base md:peer-focus:text-base peer-focus:text-sm peer-placeholder-shown:text-teal peer-placeholder-shown:translate-y-6 peer-placeholder-shown:font-medium  peer-focus:-translate-y-0  transition-all duration-200'
+                >
+                  Your Name
+                </label>
+              </div>
+              <div className='relative'>
+                <input
+                  type='email'
+                  name='email'
+                  id='email'
+                  className='p-3 bg-transparent border border-gray-200 rounded-[.5rem] outline-none peer w-full placeholder-transparent focus:border-teal'
+                  placeholder='email'
+                  required
+                />
+                <label
+                  htmlFor='email'
+                  className='absolute left-3 -top-3 cursor-text  bg-sage px-1 text-base lg:peer-focus:text-base md:peer-focus:text-base peer-focus:text-sm peer-placeholder-shown:text-teal peer-placeholder-shown:translate-y-6 peer-placeholder-shown:font-medium  peer-focus:-translate-y-0  transition-all duration-200'
+                >
+                  Email
+                </label>
+              </div>
+              <div className='relative'>
+                <input
+                  type='text'
+                  name='message'
+                  id='message'
+                  className='p-3 bg-transparent border border-gray-200 rounded-[.5rem] outline-none peer w-full placeholder-transparent focus:border-teal'
+                  placeholder='message'
+                  required
+                />
+                <label
+                  htmlFor='message'
+                  className='absolute left-3 -top-3 cursor-text  bg-sage px-1 text-base lg:peer-focus:text-base md:peer-focus:text-base peer-focus:text-sm peer-placeholder-shown:text-teal peer-placeholder-shown:translate-y-6 peer-placeholder-shown:font-medium  peer-focus:-translate-y-0  transition-all duration-200'
+                >
+                  Message
+                </label>
+              </div>
+              <button className='btn w-full'>
+                {loading ? 'Sending...' : 'Send Message'}
+              </button>
+            </form>
+            <p className='mt-2 text-base-100'>
+              *We won&apos;t share your data with anyone else...
+            </p>
+          </div>
+          {axiosError && (
+            <div className='toast toast-center toast-middle'>
+              <div className='alert alert-info'>
+                <span>{axiosError.message}</span>
+              </div>
+            </div>
+          )}
+          {posted && (
+            <div className='toast toast-center toast-middle'>
+              <div className='alert alert-success'>
+                <span>Request Successful!</span>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import SelfBanner from "@/Components/SelfBanner/page";
+import { FaNode } from "react-icons/fa6";
+import { SiJavascript, SiMongodb, SiReact } from "react-icons/si";
 
 export default function Home() {
   return (
     <div className="container">
       <SelfBanner />
+      {/* About */}
       <div className="container bg-teal-light p-10 text-center my-3 rounded-md">
         <h2 className="text-base-200 text-4xl font-semibold my-10">About Me</h2>
         <p className="text-base-100 text-lg w-3/5 mx-auto mb-8">Experienced in machine learning projects, I excel in creating intuitive user experiences and integrating complex systems. Passionate about innovation and problem-solving, I thrive in collaborative environments.</p>
@@ -14,6 +17,35 @@ export default function Home() {
           <li><p>From: <br /> <span className="font-semibold">Mymensingh, Bangladesh</span></p></li>
           <li></li>
         </ul>
+      </div>
+      {/* What I do */}
+      <div className="container">
+        <div className="p-10">
+          <h2 className="text-4xl text-base-200 font-semibold text-center mb-3">What I Do</h2>
+          <p className="text-center text-base-100 w-3/4 mx-auto mb-5">I have more than 10 years experience building software for clients all over the world. Below is a quick overview of my main technical skill sets and technologies i use. Want to find out more about my experience? Check out my online resume and project portfolio.</p>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10">
+            <div className="card">
+              <SiJavascript className="text-5xl text-teal" />
+              <h3 className="text-2xl text-base-200 font-semibold">Frontend Development</h3>
+              <p className="text-base-100">I specialize in creating responsive, user-friendly websites and applications using modern technologies like React, Next.js, and TailwindCSS.</p>
+            </div>
+            <div className="card">
+              <SiReact className="text-5xl text-teal" />
+              <h3 className="text-2xl text-base-200 font-semibold">Backend Development</h3>
+              <p className="text-base-100">I have experience building backend systems with Node.js, Express, and MongoDB. I can create RESTful APIs and integrate third-party services.</p>
+            </div>
+            <div className="card">
+              <FaNode className="text-5xl text-teal" />
+              <h3 className="text-2xl text-base-200 font-semibold">Machine Learning</h3>
+              <p className="text-base-100">I have worked on machine learning projects using Python, TensorFlow, and scikit-learn. I can build and deploy models for various applications.</p>
+            </div>
+            <div className="card">
+              <SiMongodb className="text-5xl text-teal" />
+              <h3 className="text-2xl text-base-200 font-semibold">Machine Learning</h3>
+              <p className="text-base-100">I have worked on machine learning projects using Python, TensorFlow, and scikit-learn. I can build and deploy models for various applications.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
