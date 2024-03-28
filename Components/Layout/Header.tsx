472e1a90ee8dae46ad775b117800b5d5a1190978
ifshad <shoronshark@gmail.com>
2024-03-28 19:11:29 +0600

    header deleted

--- Components/Layout/Header.tsx
+++ Components/Layout/Header.tsx
@@ -6,38 +6,6 @@ export default function Header() {
     <div className="container">
       <div className="navbar">
         <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow text-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="/about">About Me</Link>
-              </li>
-              <li>
-                <Link href="/contact">Contact</Link>
-              </li>
-            </ul>
-          </div>
           <Link href="/" className="btn btn-ghost text-xl">Ifshad</Link>
         </div>
         <div className="navbar-end">
