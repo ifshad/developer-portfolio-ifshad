b41973af4e26c22bcfaf773d6e600b80f50c89cb
ifshad <shoronshark@gmail.com>
2024-03-28 16:35:22 +0600

    about section created

--- Components/SelfBanner/page.tsx
+++ Components/SelfBanner/page.tsx
@@ -8,16 +8,16 @@ import { MdCall } from "react-icons/md";
 
 export default function SelfBanner() {
   return (
-    <div className="container flex justify-between items-center">
+    <div className="container flex justify-between items-center py-10">
       <div className="">
-        <h1 className="text-6xl text-teal font-bold">
+        <h1 className="text-6xl text-teal font-bold mb-5">
           Hi, I&apos;m <br />{" "}
           <span className="text-7xl text-navy">Ifshad Hasan Sharan</span>
         </h1>
-        <p className="text-xl text-base-100">
+        <p className="text-xl text-base-100 mb-5">
           I&apos;m a I am a versatile web developer with expertise in front-end engineering and a background in mechatronics engineering. 
         </p>
-        <div className="mt-5 flex items-center gap-5">
+        <div className="mt-10 flex items-center gap-5">
           <Link href="/contact" className="text-base-100 hover:text-navy flex items-center border-2 border-teal rounded-lg p-3 gap-2"><MdCall /><span>Contact Me</span></Link>
           <button className="btn flex items-center gap-2"><FaDownload /><span>Download CV</span></button>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,17 @@ export default function Home() {
   return (
     <div className="container">
       <SelfBanner />
+      <div className="container bg-teal-light p-10 text-center my-3 rounded-md">
+        <h2 className="text-base-200 text-4xl font-semibold my-10">About Me</h2>
+        <p className="text-base-100 text-lg w-3/5 mx-auto mb-8">Experienced in machine learning projects, I excel in creating intuitive user experiences and integrating complex systems. Passionate about innovation and problem-solving, I thrive in collaborative environments.</p>
+        <ul className="flex justify-between text-base-100 w-3/4 mx-auto my-10">
+          <li><p>Name: <br /> <span className="font-semibold">Ifshad Hasan Sharan</span></p></li>
+          <li><p>Email: <br /> <span className="font-semibold">ifshadmte.51@gmail.com</span></p></li>
+          <li><p>Date of Birth: <br /> <span className="font-semibold">28 February, 2001</span></p></li>
+          <li><p>From: <br /> <span className="font-semibold">Mymensingh, Bangladesh</span></p></li>
+          <li></li>
+        </ul>
+      </div>
     </div>
   );
 }
