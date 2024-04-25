1b35d93dc74e495f30bb13a0f16c554adc1a0965
ifshad <shoronshark@gmail.com>
2024-04-25 14:26:17 +0600

    volunteer added

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import EducationPage from "@/Components/EducationPage/page";
 import ExtraCurActPage from "@/Components/ExtraCurActPage/page";
 import ProfessionalExpPage from "@/Components/ProfessionalExpPage/page";
 import SelfBanner from "@/Components/SelfBanner/page";
+import VolunteerPage from "@/Components/VolunteerPage/page";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <ProfessionalExpPage />
       <ExtraCurActPage />
       <AchievementPage />
+      <VolunteerPage />
     </div>
   );
 }
